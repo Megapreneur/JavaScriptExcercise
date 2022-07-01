c7315ad440724859075b083fedd7b5850d84c18e
Megapreneur <ademolamegbabi@gmail.com>
2022-07-01 14:24:05 +0100

    Having  some fun with JavaScript

--- MostFrequentItemOfAnArray.js
+++ MostFrequentItemOfAnArray.js
@@ -1,8 +1,15 @@
-var arr1 = [3, 'b', 'a', 'a', 'f', 'f', 'b', 3, 'a', 'b', 4, 'b', 3];
-function mostFrequentItemOfAnArray(arr1){
-    for (let i = 0; i < arr1.length; i++) {
+let arr1 = [3, 'b', 'a', 'a', 'f', 'f', 'b', 3, 'a', 'b', 4, 'b', 3];
+function mostFrequentItemOfAnArray(arr){
 
-
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0;
+        for (let j = 0; j < arr.length; j++) {
+            if (arr.indexOf([i]) === arr[j]){
+                count += 1;
+            }
+        }
+        console.log(count)
     }
-
 }
+
+mostFrequentItemOfAnArray(arr1)
