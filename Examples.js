707c36f775db814b23414ab3660b0808044a3c3a
Megapreneur <ademolamegbabi@gmail.com>
2022-07-03 17:53:30 +0100

    Having  some fun with JavaScript

---
+++ Examples.js
@@ -0,0 +1,9 @@
+const hummus = function (factor){
+    const ingredient = function(amount, unit, name){
+        let ingredientAmount = amount * factor;
+        if (ingredientAmount > 1){
+            unit += "s"
+        };
+        console.log()
+    }
+}
